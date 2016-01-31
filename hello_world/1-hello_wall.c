36e05dd7ddba4e51e0dd416648a4e8f4d0a16a06
Kris Bredemeier <34@holbertonschool.com>
2016-01-31 10:05:10 -0800

    remvoes recursive files

--- hello_world/1-hello_wall.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main ()
-{
-        puts("Hello, C\n");
-        return  0;
-}
-
-
--- hello_world/3-hello_write.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main ()
-{
-        puts("Hello, C");
-        return  0;
-}
-
